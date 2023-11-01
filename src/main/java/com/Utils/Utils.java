b8ce126156cf65003cb005136280950c8497ef68
Admin <Admin@DESKTOP-24KG73K>
2023-11-01 09:49:46 +0530

    admin

--- src/main/java/com/Utils/Utils.java
+++ src/main/java/com/Utils/Utils.java
@@ -1,5 +1,50 @@
 package com.Utils;
 
-public class Utils {
 
-}
+	import java.io.File;
+	import java.io.FileInputStream;
+	import java.io.IOException;
+
+	import org.apache.poi.ss.usermodel.DataFormatter;
+	import org.apache.poi.xssf.usermodel.XSSFSheet;
+	import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+	import org.openqa.selenium.WebDriver;
+
+	import com.base.Testbase;
+
+	public class Utils extends Testbase{
+
+			public Utils() throws Throwable {
+			super();
+			// TODO Auto-generated constructor stub
+		}
+
+			public Utils(WebDriver driver) throws Throwable {
+				super();
+				// TODO Auto-generated constructor stub
+			}
+
+		public  static  Object[][]   getdatafromexcel(String sheetname) throws IOException
+		{
+			File file=new File("./src/main/java/com/base/Book_testng-task-1.xlsx");
+			FileInputStream fis=new FileInputStream(file);
+			XSSFWorkbook book=new XSSFWorkbook(fis);
+			XSSFSheet sheet=book.getSheet(sheetname);
+			int rownum=sheet.getPhysicalNumberOfRows();
+			int cellnum=sheet.getRow(1).getLastCellNum();
+			Object[][] data =new Object[rownum-1][cellnum];
+			for(int i=0;i<rownum-1;i++)
+			{ 
+				for(int j=0;j<cellnum;j++)
+				{
+					DataFormatter format=new DataFormatter();
+					data[i][j]=format.formatCellValue(sheet.getRow(i+1).getCell(j));
+				}
+				
+			}
+			return data; 
+		}
+
+	}
+
+
